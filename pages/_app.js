dec435895a35ee1905cb1f2a30f17177f036f73f
FelixKLG <felix@felixklg.dev>
2021-12-11 21:30:02 +0000

    Add simple <NextHead> tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react";
+import HeadTags from "../components/UI/HeadTags";
 
 function MyApp({ Component, pageProps: { session, ...pageProps} }) {
   return (
-      <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <SessionProvider session={session} refetchInterval={5 * 60}>
+          <HeadTags/>
+          <Component {...pageProps} />
       </SessionProvider>
   )
 }
