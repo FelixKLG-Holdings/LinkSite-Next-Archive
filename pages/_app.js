8d74454521ec4cddc8e5b7b8a3cd009142de439b
FelixKLG <felix@felixklg.dev>
2021-12-15 17:19:37 +0000

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 import '../styles/globals.css'
-import { SessionProvider } from "next-auth/react";
-import HeadTags from "../components/UI/HeadTags";
+import {SessionProvider} from "next-auth/react";
+import HeadTags from "../components/HeadTags";
+import LoadFonts from "../components/fonts";
 
-function MyApp({ Component, pageProps: { session, ...pageProps} }) {
-  return (
-      <SessionProvider session={session} refetchInterval={5 * 60}>
-          <HeadTags/>
-          <Component {...pageProps} />
-      </SessionProvider>
-  )
+function MyApp({Component, pageProps: {session, ...pageProps}}) {
+    return (
+        <SessionProvider session={session} refetchInterval={5 * 60}>
+            <HeadTags/>
+            <LoadFonts/>
+            <Component {...pageProps} />
+        </SessionProvider>
+    )
 }
 
 export default MyApp
