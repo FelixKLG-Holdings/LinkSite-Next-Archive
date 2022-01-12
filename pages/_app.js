5a9e74b84f942fb8390335c867c9ff74b179acfd
FelixKLG <felix@felixklg.dev>
2022-01-12 02:30:19 +0000

    Remove NextAuth
    I have to make my own own solution as nextauth is pain

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
 import '../styles/globals.css'
-import {SessionProvider} from "next-auth/react";
 import HeadTags from "../components/HeadTags";
 import LoadFonts from "../components/fonts";
 
-function MyApp({Component, pageProps: {session, ...pageProps}}) {
+function MyApp({ Component, pageProps }) {
     return (
-        <SessionProvider session={session} refetchInterval={5 * 60}>
-            <HeadTags/>
-            <LoadFonts/>
-            <Component {...pageProps} />
-        </SessionProvider>
+        <HeadTags/>
+        <LoadFonts />
+        <Component {...pageProps} />
     )
 }
 
