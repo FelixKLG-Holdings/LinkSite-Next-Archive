b88b62eacbe2053c2f0767d314f1881ce5e1cdaf
FelixKLG <felix@felixklg.dev>
2022-01-22 20:10:30 +0000

    removing crap

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,7 @@
 import '../styles/globals.css'
-import HeadTags from "../components/HeadTags";
-import LoadFonts from "../components/fonts";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <HeadTags/>
-        <LoadFonts />
         <Component {...pageProps} />
     )
 }
