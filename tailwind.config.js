1f20f59c9d5e8daa034c563922af65b8b7e659ef
FelixKLG <felix@felixklg.dev>
2022-01-22 20:09:17 +0000

    Update Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mode: 'jit',
     content: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
@@ -8,7 +7,6 @@ module.exports = {
         extend: {},
     },
     plugins: [
-        require('@tailwindcss/forms'),
         require('@tailwindcss/typography')
     ],
 }
