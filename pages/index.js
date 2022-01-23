b7fec834ede814c3fe40e24d3ea4cef9156ff92e
FelixKLG <felix@felixklg.dev>
2022-01-23 06:08:03 +0000

    login button

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Link from 'next/link'
+
 export default function index() {
     return (
-        <div className={"border-gray-900 h-screen"}>
-            <p>
-                This is a simple test :)
-            </p>
+        <div className={"bg-slate-700 h-screen text-gray-100 text-center text-9xl"}>
+            <Link href={"/api/auth/login"}>
+                    click here to log in!
+            </Link>
         </div>
     )
 }
