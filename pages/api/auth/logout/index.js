f85b0c825e618ea3f4cab8a228acedaf1e81b0fd
FelixKLG <felix@felixklg.dev>
2022-01-23 13:31:23 +0000

    Add logout route

---
+++ pages/api/auth/logout/index.js
@@ -0,0 +1,8 @@
+import { withSessionRoute } from "lib/auth/withSession";
+
+export default withSessionRoute(user)
+
+async function user(req, res) {
+    await req.session.destroy();
+    await res.status(200).send('logged out');
+}
