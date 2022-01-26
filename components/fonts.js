ccad6cbf1e9d7de7e4393d98d70cd9e0371a8c49
FelixKLG <felix@felixklg.dev>
2022-01-26 00:27:46 +0000

    Remove useless components

--- components/fonts.js
+++
@@ -1,9 +0,0 @@
-import Script from "next/script";
-import styles from '../styles/fonts.module.css';
-
-
-export default function LoadFonts() {
-    return <>
-        <Script src={"/js/TypeKit.js"} strategy={"afterInteractive"}/>
-    </>
-}
