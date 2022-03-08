819f5c50047c6589db7e06aca287cdc3420650a6
FelixKLG <felix@felixklg.dev>
2022-03-08 01:40:48 +0000

    discord info

--- pages/api/connections/discord/index.js
+++ pages/api/connections/discord/index.js
@@ -1,3 +1,20 @@
-export default async function oauth(req, res) {
-	res.redirect(307, process.env.DSICORD_OAUTH_URL);
+import { getDiscord } from '/lib/auth/Discord/database';
+import {withSessionRoute} from "/lib/auth/user/withSession";
+
+export default withSessionRoute(getDiscordAccount);
+
+async function getDiscordAccount(req, res) {
+
+	const discordAccount = await getDiscord(req.session.account);
+
+	if (!discordAccount.discord) {
+		res.status(404).json({error: 'You have no linked Discord account.'});
+		return;
+	}
+
+	const json = JSON.stringify(discordAccount, (key, value) =>
+		typeof value === "bigint" ? value.toString() : value
+	);
+
+	res.status(200).json(JSON.parse(json))
 }
